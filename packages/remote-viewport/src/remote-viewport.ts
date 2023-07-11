dd75ff0880bbf983f19b2835c51e96c376dc39bd
Paul Elliott <paul.elliott@kitware.com>
2023-07-11 13:47:45 -0400

    WIP add hypha remote-viewport rendering

--- packages/remote-viewport/src/remote-viewport.ts
+++ packages/remote-viewport/src/remote-viewport.ts
@@ -1,3 +1,4 @@
+import { hyphaWebsocketClient } from 'imjoy-rpc';
 import { createViewport as parentCreateViewport } from '@itk-viewer/viewer/viewport.js';
 
 export const createViewport = ({
@@ -8,6 +9,22 @@ export const createViewport = ({
   address: string;
 }) => {
   parent.innerHTML = `<div>Remote viewport at ${address}</div>`;
+
+  hyphaWebsocketClient
+    .connectToServer({
+      server_url: 'https://ai.imjoy.io',
+    })
+    .then(async (api) => {
+      const renderer = await api.getService('agave-ai4life');
+      console.log('Renderer', renderer);
+
+      renderer.hello();
+
+      renderer.setup();
+      renderer.run();
+      // const frame = await renderer.render();
+      // console.log('Frame', frame);
+    });
 
   return parentCreateViewport();
 };
